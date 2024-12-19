c30d7484718de41703af15f3c7b5e74fad0b63c5
Valentine Chimefo <valentinechimefo@gmail.com>
2024-12-19 11:37:19 +0100

    4 - getMyConversations Query added

--- convex/schema.ts
+++ convex/schema.ts
@@ -21,4 +21,11 @@ export default defineSchema({
         admin: v.optional(v.id("users")),
     }),
 
+    messages: defineTable({
+        conversation: v.id("conversations"),
+        sender: v.string(), // should be a string to not throw errors in openai part (ChatGPT)
+        content: v.string(),
+        messageType: v.union(v.literal("text"), v.literal("image"), v.literal("video")),
+    }).index("by_conversation", ["conversation"]),
+
 });
