788563d08d2c723017e471be3a336682920fb192
Valentine Chimefo <valentinechimefo@gmail.com>
2024-12-23 15:14:40 +0100

    4.3 - getMessagesQuery Added

--- convex/messages.ts
+++ convex/messages.ts
@@ -1,5 +1,5 @@
 import { ConvexError, v } from "convex/values";
-import { mutation } from "./_generated/server";
+import { mutation, query } from "./_generated/server";
 
 export const sendTextMessage = mutation({
     args: {
@@ -42,4 +42,72 @@ export const sendTextMessage = mutation({
 
         // TODO => add @gtp check later
     }
+});
+
+
+// Optimized
+export const getMessages = query({
+    args: {
+        conversation: v.id("conversations"),
+    },
+    handler: async(ctx, args) => {
+        const identity = await ctx.auth.getUserIdentity();
+        if(!identity) {
+            throw new ConvexError("Not authenticated");
+        }
+
+        const messages = await ctx.db.query("messages")
+        .withIndex("by_conversation", (q) => q.eq("conversation", args.conversation)).collect();
+
+        const userProfileCache = new Map();
+
+        const messageWithSender = await Promise.all(
+            messages.map(async (message) => {
+                let sender;
+                // Check if sender profile is in cache
+                if(userProfileCache.has(message.sender)){
+                    sender = userProfileCache.get(message.sender);
+                }else{
+                    // Fetch sender profile from database
+                    sender = await ctx.db.query("users")
+                    .filter(q => q.eq(q.field("_id"), message.sender))
+                    .first()
+                    // Cache the sender profile
+                    userProfileCache.set(message.sender, sender)
+                }
+
+                return {...message, sender}
+            })
+        )
+        return messageWithSender;
+    }
 })
+
+
+
+// unoptimized 
+// export const getMessages = query({
+//     args: {
+//         conversation: v.id("conversations"),
+//     },
+//     handler: async(ctx, args) => {
+//         const identity = await ctx.auth.getUserIdentity();
+//         if(!identity) {
+//             throw new ConvexError("Not authenticated");
+//         }
+
+//         const messages = await ctx.db.query("messages")
+//         .withIndex("by_conversation", q=> q.eq("conversation", args.conversation)). collect();
+
+//         const messageWithSender = await Promise.all(
+//             messages.map(async (message) => {
+//                 const sender = await ctx.db.query("users")
+//                 .filter(q => q.eq(q.field("_id"), message.sender))
+//                 .first()
+
+//                 return {...message, sender}
+//             })
+//         )
+//         return messageWithSender;
+//     }
+// })
